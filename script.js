5cc122ee222ca1e65812e639dd91db7eee3a8a72
horvathlillareka <horvathlillareka@edu.bme.hu>
2024-12-07 21:50:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
-document.addEventListener("DOMContentLoaded", () => {
-    fetch("https://randomuser.me/api/")
+ {
+    fetch('https://randomuser.me/api/')
         .then(response => response.json())
         .then(data => {
-            const user = data.results[0];
-            
-            // Elemszerkesztés a kép nélkül
-            const userInfo = document.getElementById("user-info");
-            userInfo.innerHTML = `
-                <h2>${user.name.first} ${user.name.last}</h2>
-                <p>Email: ${user.email}</p>
-                <p>Telefon: ${user.phone}</p>
-                <p>Ország: ${user.location.country}</p>
-            `;
+            img.src = data.results[0].picture.large;
+            aboutSection.prepend(img);
         })
-        .catch(error => console.error("Hiba történt az API lekérésekor:", error));
+        .catch(error => console.error('Hiba történt az API betöltésekor:', error));
 });
+
