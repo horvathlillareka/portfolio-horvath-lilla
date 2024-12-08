ec89d44c9c95c471eacd9f88308066622399147a
horvathlillareka <horvathlillareka@edu.bme.hu>
2024-12-08 21:19:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,5 @@ document.addEventListener("DOMContentLoaded", () => {
         .then(data => {
             const user = data.results[0];
             
-            // Lekért adatok megjelenítése KÉP nélkül
-            const userInfo = document.getElementById("user-info");
-            userInfo.innerHTML = `
-                <h2>${user.name.first} ${user.name.last}</h2>
-                <p><strong>Email:</strong> ${user.email}</p>
-                <p><strong>Telefon:</strong> ${user.phone}</p>
-                <p><strong>Ország:</strong> ${user.location.country}</p>
-            `;
-        })
         .catch(error => console.error("Hiba történt az API lekérésekor:", error));
 });
